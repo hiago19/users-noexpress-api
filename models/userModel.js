94c71cf04647974ea1885044fb1371625c9734ff
Bruno Hiago <bhiago99@gmail.com>
2025-03-28 17:50:01 -0300

    Altera dados de usuário no modelo para fins de teste

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 // Simulação de um "banco de dados"
 export const users = [
-    { id: 1, nome: 'Bruno', sobrenome: 'Hiago', idade: 25 },
+    { id: 1, nome: 'teste', sobrenome: 'teste', idade: 25 },
 ];
 
 // Função para gerar um novo ID
